2e8cfe9673c755edd871e3dc1b4069dc135eba01
cbt_java@163.com <cbt_java@163.com>
2019-05-07 23:30:30 -0700

    update

--- src/main/java/com/example/controller/TestListener.java
+++ src/main/java/com/example/controller/TestListener.java
@@ -12,8 +12,6 @@ public class TestListener {
 
     @JmsListener(destination = "testmq789456")
     public void receive(Object msg) throws JMSException {
-
-        
         TextMessage textMessage = (TextMessage) msg;
         System.out.println("收到消息====" + textMessage.getText());
 
